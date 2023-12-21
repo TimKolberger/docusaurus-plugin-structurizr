387a5eed8caaaaf82651371d85cf515ee1679671
Tim Kolberger <tim@kolberger.eu>
2023-12-21 10:34:22 +0100

    chore: prepare mono repo

--- packages/docusaurus-plugin-structurizr/.eslintrc.js
+++ packages/docusaurus-plugin-structurizr/.eslintrc.js
@@ -1,10 +1,9 @@
-// This configuration only applies to the package manager root.
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  ignorePatterns: ["apps/**", "packages/**"],
+  root: true,
   extends: ["@dps/eslint-config/library.js"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: true,
+    project: "./tsconfig.lint.json",
   },
 };
